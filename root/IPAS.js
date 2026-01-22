005165c7ae5c17b7d30c46fc2d2372e6cb54a35f
Jeomatique <julian.rene.joaquim@gmail.com>
2026-01-22 22:32:04 +0100

    Update IPAS.js

--- root/IPAS.js
+++ root/IPAS.js
@@ -2,37 +2,55 @@ function IPAS(feature) {
   if (feature.properties['class_et'] === '< -1') {
     return {
           "fillColor": "#900002",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['class_et'] === '-1 - -0.5') {
     return {
           "fillColor": "#b94d43",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['class_et'] === '-0.5 - 0') {
     return {
           "fillColor": "#f8d664",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['class_et'] === '0 - 0.5') {
     return {
           "fillColor": "#95e774",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['class_et'] === '0.5 - 1') {
     return {
           "fillColor": "#58ddd6",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['class_et'] === '>= 1') {
     return {
           "fillColor": "#1b84d4",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#383838",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   return {
