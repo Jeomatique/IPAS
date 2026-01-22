4029d1fdc6ffafeef6866440f4949c908f16d875
Jeomatique <julian.rene.joaquim@gmail.com>
2026-01-22 22:32:55 +0100

    Update IRIS_PARIS_revenu.js

--- root/IRIS_PARIS_revenu.js
+++ root/IRIS_PARIS_revenu.js
@@ -2,31 +2,46 @@ function IRIS_PARIS_revenu(feature) {
   if (feature.properties['MED21'] >= 0 && feature.properties['MED21'] <= 15342.80000000000109139) {
     return {
           "fillColor": "#f6f6f6",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#666666",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['MED21'] > 15342.80000000000109139 && feature.properties['MED21'] <= 29674.80000000000291038) {
     return {
           "fillColor": "#b9e1e5",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#666666",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['MED21'] > 29674.80000000000291038 && feature.properties['MED21'] <= 37431.19999999999708962) {
     return {
           "fillColor": "#7bccd4",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#666666",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['MED21'] > 37431.19999999999708962 && feature.properties['MED21'] <= 45465.80000000000291038) {
     return {
           "fillColor": "#3eb7c4",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#666666",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   else if (feature.properties['MED21'] > 45465.80000000000291038 && feature.properties['MED21'] <= 124575) {
     return {
           "fillColor": "#00a1b3",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#666666",
+          "weight": 1,
+          "opacity": 0.5
     };
   }
   return {
