1cbd5305c9d22d257cb982be5e4f5ed42c5a97f2
Jeomatique <julian.rene.joaquim@gmail.com>
2026-01-22 22:35:05 +0100

    Update Seine.js

--- root/Seine.js
+++ root/Seine.js
@@ -2,7 +2,10 @@ function Seine(feature) {
   if (true) {
     return {
           "fillColor": "#68a9df",
-          "fillOpacity": 1.0
+          "fillOpacity": 1.0,
+          "color": "#2e5f8a",
+          "weight": 1,
+          "opacity": 0.8
     };
   }
   return {
