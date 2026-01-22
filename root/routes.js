e81e94c99a576c46ea1da49069b9ed44984738cd
Jeomatique <julian.rene.joaquim@gmail.com>
2026-01-22 22:34:39 +0100

    Update routes.js

--- root/routes.js
+++ root/routes.js
@@ -1,33 +1,35 @@
 function routes(feature) {
-  if (feature.properties['IMPORTANCE'] === '2' && feature.properties['IMPORTANCE'] === '3') {
+  if (feature.properties['IMPORTANCE'] === '2' || feature.properties['IMPORTANCE'] === '3') {
     return {
           "color": "#6e6e6e",
-          "weight": 1.0
+          "weight": 1.0,
+          "opacity": 1.0
     };
   }
   else if (feature.properties['IMPORTANCE'] === '4') {
     return {
           "color": "#000000",
-          "weight": 2.0
+          "weight": 2.0,
+          "opacity": 1.0
     };
   }
-  else if (feature.properties['IMPORTANCE'] === '5' && feature.properties['IMPORTANCE'] === '6') {
+  else if (feature.properties['IMPORTANCE'] === '5' || feature.properties['IMPORTANCE'] === '6') {
     return {
           "color": "#a0a0a0",
-          "weight": 1.0
+          "weight": 1.0,
+          "opacity": 1.0
     };
   }
   else if (feature.properties['layer'] === '2') {
     return {
           "color": "#000000",
-          "weight": 6.0
+          "weight": 6.0,
+          "opacity": 1.0
     };
   }
   return {
-    fillColor: '#cccccc',
-    fillOpacity: 0.7,
-    color: '#000000',
-    weight: 1,
-    opacity: 1
+    color: '#999999',
+    weight: 0.5,
+    opacity: 0.5
   };
 }
